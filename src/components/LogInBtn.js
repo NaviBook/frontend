b8e08ac700b9848d0a8440b7e27e5fc65d3bd69e
ajin <aj5210@naver.com>
2023-01-30 16:40:11 +0900

    Create main page^-^

---
+++ src/components/LogInBtn.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function LogInBtn() {
+  return(
+    <div className="container">
+        <Link href="/login" legacyBehavior>
+            <a>로그인</a>
+        </Link>
+        <style jsx>{`
+            .container {
+            height: 60px;
+            display: flex;
+            flex-direction: row-reverse;
+            align-items: center;
+            padding: 0px 50px;
+            }
+            
+            a {
+            text-decoration: none;
+            color: black;
+            border: 1px solid black;
+            padding: 5px 10px;
+            }
+        `}</style>
+    </div>
+  );
+}
